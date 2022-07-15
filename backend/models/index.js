22dafb303571047aedbe1d56b8fbbd8c1bea385a
BushraFatimaBF <bushrafatima23@gmail.com>
2022-07-15 14:18:32 +0100

    connecting to postman

--- backend/models/index.js
+++ backend/models/index.js
@@ -7,3 +7,10 @@ const res = await query(`SELECT * FROM todo_list;`);
 const notesArr= res.rows;
 return notesArr
 }
+
+export async function postItem(info){
+    const res = await query(`INSERT INTO todo_list (id, item)
+    VALUES ($1, $2);`, [info.id, info.item])
+    const newItem = res.rows;
+    return newItem
+}
